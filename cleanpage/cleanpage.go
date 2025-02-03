1299a254c5aeea726ea010cb6c50cf8c5db9661c
Julian Shen <julianshen@gmail.com>
2025-02-03 22:54:03 +0800

    Use chrome to scrape page in order to pull SPA content

--- cleanpage/cleanpage.go
+++ cleanpage/cleanpage.go
@@ -1,20 +1,27 @@
 package cleanpage
 
 import (
+	"context"
 	"io"
 	"net/http"
 	"net/url"
 	"strings"
 	"time"
 
+	"github.com/chromedp/chromedp"
 	"github.com/go-shiori/go-readability"
 )
 
 // CrawlPage downloads a webpage and extracts its main content
 func CrawlPage(urlStr string) (string, error) {
-	html, err := downloadWithHTTP(urlStr)
+	// Try Chrome first
+	html, err := downloadWithChrome(urlStr)
 	if err != nil {
-		return "", err
+		// Fall back to HTTP client
+		html, err = downloadWithHTTP(urlStr)
+		if err != nil {
+			return "", err
+		}
 	}
 
 	// Parse URL string into *url.URL
@@ -51,4 +58,30 @@ func downloadWithHTTP(urlStr string) (string, error) {
 	}
 
 	return string(body), nil
+}
+
+func downloadWithChrome(urlStr string) (string, error) {
+	// Create context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Create a timeout
+	ctx, cancel = context.WithTimeout(ctx, 30*time.Second)
+	defer cancel()
+
+	var html string
+	err := chromedp.Run(ctx,
+		// Navigate to the page
+		chromedp.Navigate(urlStr),
+		// Wait for the page to load
+		chromedp.WaitReady("body"),
+		// Extract the HTML
+		chromedp.OuterHTML("html", &html),
+	)
+
+	if err != nil {
+		return "", err
+	}
+
+	return html, nil
 }
